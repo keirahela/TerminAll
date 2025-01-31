1a3ee3c95839506c05912736b5e01169b2198b5d
keirahela <mirkamarkate@gmail.com>
2025-01-31 14:57:15 +0100

    rm, new file system

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ let userInput = document.getElementById("userinput");
 let history = document.getElementById("history");
 let currentPath = document.getElementById("currentPath");
 let currentDirectory = "/";
+let BASE_PREFIX = "terminal@rm-rf";
 let fileSystem = {
-  "/": ["home", "etc", "user", "README.txt"],
-  "/home": ["user1", "user2", "public"],
-  "/home/user1": ["documents", "downloads", "pictures"],
-  "/home/user2": ["projects", "music"],
-  "/etc": ["config1", "config2"],
-  "/user": ["file1.txt", "file2.txt"],
-  "/home/user1/documents": ["doc1.txt", "doc2.txt"],
+  home: {
+    user1: {
+      documents: ["doc1.txt", "doc2.txt"],
+      downloads: [],
+      pictures: [],
+    },
+    user2: {
+      projects: [],
+      music: [],
+    },
+    public: [],
+  },
+  etc: {
+    config1: [],
+    config2: [],
+  },
+  user: ["file1.txt", "file2.txt"],
+  "README.txt": [],
 };
 let commands = [
   "cal",
@@ -149,54 +161,106 @@ function clearInput() {
 }
 
 function ls() {
-  if (fileSystem[currentDirectory]) {
-    return fileSystem[currentDirectory].join("\n");
+  const currentDirObj = getCurrentDirectoryObject(currentDirectory);
+
+  if (currentDirObj) {
+    const entries = Object.keys(currentDirObj);
+    return entries.length > 0
+      ? entries.join("\n")
+      : "Empty directory: " + currentDirectory;
   } else {
     return "No such directory: " + currentDirectory;
   }
 }
 
+function getCurrentDirectoryObject(currentDir) {
+  const parts = currentDir.split("/").filter(Boolean);
+  let current = fileSystem;
+
+  for (const part of parts) {
+    if (current[part]) {
+      current = current[part];
+    } else {
+      return null;
+    }
+  }
+
+  return current;
+}
+
 function cd(directory) {
+  const currentDirObj = getCurrentDirectoryObject(currentDirectory);
+
   if (directory === "..") {
     const parentDir = currentDirectory.substring(
       0,
       currentDirectory.lastIndexOf("/")
     );
     currentDirectory = parentDir === "" ? "/" : parentDir;
-    currentPath.innerText = `terminal@rm-rf ${
-      currentDirectory.split("/").pop() == ""
-        ? "~"
-        : currentDirectory.split("/").pop()
-    } $`;
+    updateCurrentPath();
     return `Changed directory to: ${currentDirectory}`;
-  }
+  } else if (directory.startsWith("/")) {
+    const parts = directory.split("/").filter(Boolean);
+    let newCurrentDir = fileSystem;
 
-  if (directory.startsWith("/")) {
-    if (fileSystem[directory]) {
-      currentDirectory = directory;
-      currentPath.innerText = `terminal@rm-rf ${
-        directory.split("/").pop() == "" ? "~" : directory.split("/").pop()
-      } $`;
-      return `Changed directory to: ${currentDirectory}`;
-    } else {
-      return `No such directory: ${directory}`;
+    for (const part of parts) {
+      if (newCurrentDir[part]) {
+        newCurrentDir = newCurrentDir[part];
+      } else {
+        return "No such directory: " + directory;
+      }
     }
+
+    currentDirectory = directory;
+    updateCurrentPath();
+    return `Changed directory to: ${currentDirectory}`;
   } else {
-    const newDirectory = currentDirectory + "/" + directory;
-    const normalizedDirectory = newDirectory.replace(/\/+/g, "/");
-
-    if (fileSystem[normalizedDirectory]) {
-      currentDirectory = normalizedDirectory;
-      currentPath.innerText = `terminal@rm-rf ${
-        currentDirectory.split("/").pop() == ""
-          ? "~"
-          : currentDirectory.split("/").pop()
-      } $`;
+    const newCurrentDir = currentDirObj[directory];
+    if (newCurrentDir) {
+      currentDirectory = resolvePath(currentDirectory, directory);
+      updateCurrentPath();
       return `Changed directory to: ${currentDirectory}`;
     } else {
-      return `No such directory: ${directory}`;
+      return "No such directory: " + directory;
     }
   }
+}
+
+function resolvePath(currentDir, directory) {
+  if (directory.startsWith("/")) {
+    return directory;
+  } else if (directory === "..") {
+    const parts = currentDir.split("/").filter(Boolean);
+    parts.pop();
+    return parts.length > 0 ? "/" + parts.join("/") : "/";
+  } else if (directory === ".") {
+    return currentDir;
+  } else {
+    return currentDir === "/" ? "/" + directory : currentDir + "/" + directory;
+  }
+}
+
+function updateCurrentPath() {
+  currentPath.innerText = `${BASE_PREFIX} ${
+    currentDirectory.split("/").pop() === ""
+      ? "~"
+      : currentDirectory.split("/").pop()
+  } $`;
+}
+
+function checkIfExists(fs, dir) {
+  const parts = dir.split("/").filter(Boolean);
+  let current = fs;
+
+  for (const part of parts) {
+    if (current[part]) {
+      current = current[part];
+    } else {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 function pwd() {
@@ -208,6 +272,33 @@ function clear() {
     history.removeChild(history.firstChild);
   }
   return "";
+}
+
+let possibleOptions = ["r", "i", "f", "v"];
+
+function removeFile(file, recursive, interactive, force, verbose) {
+  console.log(file, recursive, interactive, force, verbose);
+}
+
+function rm(command) {
+  let args = command.split(" ");
+  args.shift();
+
+  let options = "";
+
+  if (args[0].includes("-")) {
+    options = args[0];
+  }
+
+  const parameters = options.split("");
+
+  removeFile(
+    args[1],
+    parameters.includes("r"),
+    parameters.includes("i"),
+    parameters.includes("f"),
+    parameters.includes("v")
+  );
 }
 
 function handleCommand(command) {
@@ -238,7 +329,9 @@ function handleCommand(command) {
     case "date":
       return date();
     case "logname":
-      return "terminal";
+      return BASE_PREFIX.split("@")[0];
+    case "rm":
+      return rm(command);
     case "":
       return "";
     default:
@@ -247,34 +340,32 @@ function handleCommand(command) {
 }
 
 function bcolor(color) {
-  if (CSS.supports('color', color)) {
+  if (CSS.supports("color", color)) {
     document.getElementById("body").style.backgroundColor = color;
     return "Background color successfully changed!";
-  }
-  else {
+  } else {
     return "Syntax error: bcolor <color>";
   }
 }
 
 function date() {
-  return new Date;
+  return new Date();
 }
 
 function echo(text) {
   let string = "";
-  for (let i = 1; text.length > i; i++ ) {
+  for (let i = 1; text.length > i; i++) {
     if (string == "") string = string + text[i];
     else string = string + " " + text[i];
-    }
+  }
   return string;
 }
 
 function fcolor(color) {
-  if (CSS.supports('color', color)) {
+  if (CSS.supports("color", color)) {
     document.getElementById("commands").style.color = color;
     return "Font color successfully changed!";
-  }
-  else {
+  } else {
     return "Syntax error: fcolor <color>";
   }
 }
@@ -286,10 +377,13 @@ function currentcar() {
 function submitCommand(command) {
   window.scrollTo(0, document.body.scrollHeight);
   clearInput();
+  let history2 = document.createElement("span");
+  history2.innerText = currentPath.innerText + " " + command;
   let span = document.createElement("span");
   span.innerText = handleCommand(command);
 
   if (command == "") return;
+  history.appendChild(history2);
   history.appendChild(span);
 }
 
